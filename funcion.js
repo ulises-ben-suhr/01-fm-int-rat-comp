cd0e8a970caf69e4076b9f21281cc617b3d5e42d
LaSudestada <ulisessuhr@hotmail.com>
2022-06-17 16:06:02 -0300

    finished

--- funcion.js
+++ funcion.js
@@ -1,7 +1,31 @@
-let formValue = document.getElementById("calif").
+const ratingContainer = document.getElementById('rating');
+const thanksContainer = document.getElementById('thanks');
 
-console.log(formValue);
+let ratingValue = document.getElementById('rating-value');
 
+const ratingButtons = document.querySelectorAll('input[name="calification"]');
+const submitButton = document.querySelector('.call-to-act');
 
+let chosenValue = -1;
 
+ratingButtons.forEach(element => {
+    element.addEventListener('click', () => {
+        chosenValue = element.value;
+    })
+})
+
+submitButton.addEventListener('click', () => {
+    if (chosenValue == -1) {
+        alert('Debe elegir una opcion');
+    }
+    else {
+        ratingValue.innerHTML = chosenValue;
+        ratingContainer.classList.remove('d-block');
+        ratingContainer.classList.add('d-none');
+
+        thanksContainer.classList.remove('d-none');
+        thanksContainer.classList.add('d-block');
+    }
+    
+})
 
